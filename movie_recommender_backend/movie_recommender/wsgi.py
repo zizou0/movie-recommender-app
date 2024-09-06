d5dda46c495b584139f91c8ee48518be3536884d
Benedict Zuzi <benedictzuzi@Benedicts-Air.attlocal.net>
2024-09-06 02:50:40 -0400

    updated wsgi settings path

--- movie_recommender_backend/movie_recommender/wsgi.py
+++ movie_recommender_backend/movie_recommender/wsgi.py
@@ -15,6 +15,6 @@ from django.core.wsgi import get_wsgi_application
 # Add your project directory to the sys.path
 sys.path.append('/app/movie-recommender/movie_recommender_backend')
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE",  "movie_recommender_backend.movie_recommender.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",  "movie-recommender.movie_recommender_backend.movie_recommender.settings")
 
 application = get_wsgi_application()
