938e6bbbf002d009e3fcb64370ef0af30ca79622
Benedict Zuzi <benedictzuzi@Benedicts-Air.attlocal.net>
2024-09-06 02:36:17 -0400

    modified installed apps in settings

--- movie_recommender_backend/movie_recommender/settings.py
+++ movie_recommender_backend/movie_recommender/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'recommender',
+    'movie-recommender.recommender',
     'rest_framework',
     'corsheaders',
 ]
