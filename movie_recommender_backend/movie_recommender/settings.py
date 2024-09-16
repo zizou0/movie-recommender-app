3dca7ec1d90b3b41dcad1a209529bfbc16e4aea5
Benedict Zuzi <benedictzuzi@Benedicts-Air.attlocal.net>
2024-09-15 20:59:09 -0400

    updated settings templates

--- movie_recommender_backend/movie_recommender/settings.py
+++ movie_recommender_backend/movie_recommender/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = "movie_recommender.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'static')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
